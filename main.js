219ebfd562880c76aa9da248f805bc102a236335
Derek Cannistraro <djdcann@gmail.com>
2023-04-13 19:53:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,532 @@
 const parent = document.getElementById("parent");
 //parent.style.height = parent.clientHeight+"px";
 
-var words1 = ["Hello", "World", "OpenAI", "ChatGPT", "Programming"]; // Your array of words
+var words1 = ["Hello", "World", "OpenAI", "ChatGPT", "Programming"];
+var story = [
+  "Alan",
+  "Tan",
+  "had",
+  "always",
+  "lived",
+  "a",
+  "charmed",
+  "life.",
+  "He",
+  "was",
+  "born",
+  "in",
+  "Singapore",
+  "to",
+  "a",
+  "wealthy",
+  "family,",
+  "and",
+  "as",
+  "the",
+  "only",
+  "son,",
+  "he",
+  "was",
+  "spoiled",
+  "by",
+  "his",
+  "parents.",
+  "He",
+  "attended",
+  "the",
+  "best",
+  "schools,",
+  "traveled",
+  "extensively,",
+  "and",
+  "lived",
+  "in",
+  "luxury.",
+  "His",
+  "parents",
+  "set",
+  "him",
+  "up",
+  "with",
+  "a",
+  "successful",
+  "career",
+  "in",
+  "finance,",
+  "and",
+  "he",
+  "quickly",
+  "rose",
+  "through",
+  "the",
+  "ranks,",
+  "becoming",
+  "a",
+  "managing",
+  "director",
+  "at",
+  "a",
+  "young",
+  "age.",
+  "As",
+  "he",
+  "approached",
+  "his",
+  "mid-thirties,",
+  "Alan",
+  "realized",
+  "that",
+  "he",
+  "wanted",
+  "more",
+  "from",
+  "life",
+  "than",
+  "just",
+  "his",
+  "successful",
+  "career.",
+  "He",
+  "longed",
+  "for",
+  "companionship,",
+  "and",
+  "he",
+  "began",
+  "to",
+  "actively",
+  "seek",
+  "out",
+  "a",
+  "partner.",
+  "He",
+  "met",
+  "Chloe,",
+  "a",
+  "beautiful",
+  "and",
+  "intelligent",
+  "woman",
+  "who",
+  "worked",
+  "in",
+  "the",
+  "same",
+  "industry.",
+  "They",
+  "quickly",
+  "fell",
+  "in",
+  "love",
+  "and",
+  "decided",
+  "to",
+  "get",
+  "married.",
+  "The",
+  "day",
+  "before",
+  "their",
+  "wedding",
+  "was",
+  "a",
+  "beautiful",
+  "day",
+  "in",
+  "Singapore.",
+  "The",
+  "sun",
+  "was",
+  "shining,",
+  "and",
+  "there",
+  "was",
+  "a",
+  "gentle",
+  "breeze",
+  "in",
+  "the",
+  "air.",
+  "Alan",
+  "spent",
+  "the",
+  "day",
+  "getting",
+  "ready",
+  "for",
+  "the",
+  "wedding",
+  "with",
+  "his",
+  "groomsmen.",
+  "They",
+  "laughed",
+  "and",
+  "joked",
+  "as",
+  "they",
+  "went",
+  "through",
+  "their",
+  "preparations.",
+  "As",
+  "the",
+  "sun",
+  "began",
+  "to",
+  "set,",
+  "Alan",
+  "went",
+  "out",
+  "for",
+  "a",
+  "walk",
+  "alone",
+  "to",
+  "clear",
+  "his",
+  "head.",
+  "He",
+  "strolled",
+  "through",
+  "the",
+  "city,",
+  "taking",
+  "in",
+  "the",
+  "sights",
+  "and",
+  "sounds",
+  "of",
+  "his",
+  "beloved",
+  "Singapore.",
+  "As",
+  "he",
+  "was",
+  "walking",
+  "down",
+  "a",
+  "quiet",
+  "street,",
+  "he",
+  "was",
+  "suddenly",
+  "attacked",
+  "from",
+  "behind.",
+  "He",
+  "didn't",
+  "see",
+  "his",
+  "attacker,",
+  "but",
+  "he",
+  "felt",
+  "a",
+  "sharp",
+  "pain",
+  "in",
+  "his",
+  "back,",
+  "and",
+  "then",
+  "everything",
+  "went",
+  "dark.",
+  "The",
+  "next",
+  "thing",
+  "Alan",
+  "knew,",
+  "he",
+  "was",
+  "lying",
+  "on",
+  "the",
+  "ground,",
+  "bleeding",
+  "profusely.",
+  "He",
+  "tried",
+  "to",
+  "get",
+  "up,",
+  "but",
+  "his",
+  "legs",
+  "wouldn't",
+  "move.",
+  "He",
+  "felt",
+  "his",
+  "strength",
+  "ebbing",
+  "away,",
+  "and",
+  "he",
+  "knew",
+  "that",
+  "he",
+  "was",
+  "dying.",
+  "As",
+  "he",
+  "lay",
+  "there,",
+  "he",
+  "thought",
+  "of",
+  "Chloe",
+  "and",
+  "how",
+  "devastated",
+  "she",
+  "would",
+  "be",
+  "when",
+  "she",
+  "found",
+  "out",
+  "what",
+  "had",
+  "happened",
+  "to",
+  "him.",
+  "The",
+  "police",
+  "arrived",
+  "soon",
+  "after",
+  "and",
+  "found",
+  "Alan",
+  "lying",
+  "in",
+  "a",
+  "pool",
+  "of",
+  "his",
+  "own",
+  "blood.",
+  "They",
+  "rushed",
+  "him",
+  "to",
+  "the",
+  "hospital,",
+  "but",
+  "it",
+  "was",
+  "too",
+  "late.",
+  "He",
+  "died",
+  "before",
+  "he",
+  "could",
+  "make",
+  "it",
+  "to",
+  "the",
+  "emergency",
+  "room.",
+  "The",
+  "news",
+  "of",
+  "Alan's",
+  "death",
+  "spread",
+  "quickly,",
+  "and",
+  "Chloe",
+  "was",
+  "devastated",
+  "when",
+  "she",
+  "heard",
+  "what",
+  "had",
+  "happened.",
+  "She",
+  "couldn't",
+  "believe",
+  "that",
+  "her",
+  "fiancé,",
+  "the",
+  "man",
+  "she",
+  "was",
+  "going",
+  "to",
+  "marry",
+  "the",
+  "next",
+  "day,",
+  "was",
+  "gone",
+  "forever.",
+  "The",
+  "police",
+  "launched",
+  "an",
+  "investigation",
+  "into",
+  "Alan's",
+  "death,",
+  "but",
+  "they",
+  "were",
+  "unable",
+  "to",
+  "find",
+  "his",
+  "killer.",
+  "The",
+  "motive",
+  "for",
+  "the",
+  "attack",
+  "was",
+  "unknown,",
+  "and",
+  "there",
+  "were",
+  "no",
+  "witnesses",
+  "to",
+  "the",
+  "crime.",
+  "Chloe",
+  "was",
+  "never",
+  "the",
+  "same",
+  "after",
+  "Alan's",
+  "death.",
+  "She",
+  "struggled",
+  "to",
+  "come",
+  "to",
+  "terms",
+  "with",
+  "her",
+  "loss",
+  "and",
+  "eventually",
+  "moved",
+  "to",
+  "another",
+  "country",
+  "to",
+  "start",
+  "a",
+  "new",
+  "life.",
+  "Alan's",
+  "parents",
+  "were",
+  "also",
+  "devastated",
+  "by",
+  "his",
+  "death",
+  "and",
+  "struggled",
+  "to",
+  "cope",
+  "with",
+  "their",
+  "grief.",
+  "Alan's",
+  "death",
+  "shocked",
+  "the",
+  "Singaporean",
+  "community,",
+  "and",
+  "it",
+  "served",
+  "as",
+  "a",
+  "reminder",
+  "that",
+  "even",
+  "the",
+  "most",
+  "charmed",
+  "lives",
+  "can",
+  "be",
+  "cut",
+  "short",
+  "by",
+  "senseless",
+  "violence.",
+  "Despite",
+  "the",
+  "efforts",
+  "of",
+  "the",
+  "police,",
+  "his",
+  "killer",
+  "was",
+  "never",
+  "found,",
+  "and",
+  "his",
+  "family",
+  "and",
+  "friends",
+  "were",
+  "left",
+  "with",
+  "a",
+  "gaping",
+  "hole",
+  "in",
+  "their",
+  "lives.",
+  "In",
+  "the",
+  "end,",
+  "Alan's",
+  "life",
+  "was",
+  "a",
+  "reminder",
+  "that",
+  "no",
+  "one",
+  "is",
+  "immune",
+  "to",
+  "tragedy,",
+  "and",
+  "that",
+  "we",
+  "should",
+  "cherish",
+  "every",
+  "moment",
+  "we",
+  "have",
+  "with",
+  "the",
+  "people",
+  "we",
+  "love.",
+];
 var words2 = [
   "inchoate",
   "lacuna lacunar",
-  "perfuse suffuse diffuse",
+  "perfuse",
+  "suffuse",
+  "diffuse",
   "embolus",
   "palaver",
   "punctate",
@@ -17,7 +538,6 @@ var words2 = [
   "mellifluous",
   "torpor",
   "tryst",
-  "postprandial tumescence",
   "gestalt",
   "habitue",
   "recalcitrant",
@@ -37,8 +557,26 @@ var words2 = [
   "vignette",
   "adulation",
 ];
-var words3 = ["渾", "廈", "王道楽土"];
+var words3 = ["渾", "廈", "王道楽土", "推しの子","後見人","実質","身元引受人","自信家","偶像","隠し通す","欲張り","図太い","野太い","帝王切開","無痛分娩","羽ばたき","産気づく","端くれ","復帰","危なっかしい","音源","打算","お偉方","酔狂","焦がす","吸い寄せる","腑に落ちる","不可抗力","超常現象","失禁","逸材","猿人","摂理","倫理","隠蔽","母子手帳","心の渇き","狼藉","化身","隈取り","中敷き　（靴の）","迫真","快進撃","中抜き","習い事","歌唱","印税","天引き","世も末","エゴサ","調律","体現","極楽浄土","改革","粗相","早熟","晩熟","広告塔","ちょびっと","仰々しい","重曹","ずぶの素人","急遽","コミュ力","大御所","お灸を据える","貴重","詮索","巷説","網膜","角膜","譲り","勝ち気","受胎","びっちり","さばける","窃盗","釈放","ないがしろ","共演","授業参観","殺害","熱狂","明け暮れ","有名税","免罪符","簡素","袋叩き","頑な","割り出す"];
 var countryFlags = [
+  "🇵🇲",
+  "🇰🇿",
+  "🇹🇫",
+  "🇧🇱",
+  "🇦🇨",
+  "🇬🇵",
+  "🇵🇳",
+  "🇨🇲",
+  "🇹🇦",
+  "🇮🇴",
+  "🇷🇪",
+  "🇲🇱",
+  "🇸🇳",
+  "🇬🇸",
+  "🇧🇯",
+  "🇹🇫",
+];
+var countryFlagsAll = [
   "🇦🇨",
   "🇦🇩",
   "🇦🇪",
@@ -296,17 +834,17 @@ var countryFlags = [
   "🇿🇼",
 ];
 
-shuffleArray(countryFlags);
+shuffleArray(countryFlagsAll);
 
 const arrayMap = {
   array1: words1,
   array2: words2,
   array3: words3,
-  array4: countryFlags,
+  array4: countryFlagsAll,
   // Add more entries for your other word arrays
 };
 
-var words = countryFlags;
+var words = words3;
 var currentWordIndex = 0;
 var displayTime = 200; // Time in milliseconds
 var isPaused = false;
@@ -363,7 +901,17 @@ function toggleCollapsible() {
 function togglePause() {
   isPaused = !isPaused;
   if (!isPaused) {
+    if (currentWordIndex == words.length - 1) {
+      currentWordIndex = 0;
+    } else {
+      currentWordIndex++;
+    }
     iterate();
+  } else {
+    currentWordIndex--;
+    if (currentWordIndex == -1) {
+      currentWordIndex = words.legnth - 1;
+    }
   }
 }
 
@@ -371,7 +919,13 @@ document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
     togglePause();
   }
-  if (event.code === "ShiftLeft") {
+  if (
+    event.code === "KeyA" &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey &&
+    !event.metaKey
+  ) {
     toggleCollapsible();
   }
   if (event.code === "ArrowLeft") {
@@ -384,12 +938,46 @@ document.addEventListener("keydown", function (event) {
       displayWord(currentWordIndex);
     }
   }
-  if (event.code === "KeyZ") {
+  if (
+    event.code === "KeyZ" &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey &&
+    !event.metaKey
+  ) {
     document.getElementById("save").innerHTML +=
       "<span>" + words[currentWordIndex] + "</span> ";
   }
-  if (event.code === "KeyX") {
+  if (
+    event.code === "KeyX" &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey &&
+    !event.metaKey
+  ) {
     document.getElementById("save").innerHTML = "";
+  }
+  if (
+    event.code === "KeyQ" &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey &&
+    !event.metaKey
+  ) {
+    words.splice(currentWordIndex, 1);
+    if (currentWordIndex == words.length) {
+      currentWordIndex = 0;
+    }
+    displayWord(currentWordIndex);
+  }
+  if (
+    event.code === "KeyC" &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey &&
+    !event.metaKey
+  ) {
+    document.getElementById("cat").innerHTML = "";
   }
   if (event.code === "ArrowRight") {
     if (isPaused) {
@@ -435,6 +1023,7 @@ arraySelector.addEventListener("change", function (event) {
   // Update the main array based on the selected option
   words = arrayMap[selectedArray];
   currentWordIndex = 0;
+  displayWord(currentWordIndex);
 });
 
 const loopCheckbox = document.getElementById("loopCheckbox");
@@ -448,7 +1037,7 @@ document.querySelector(".icon").addEventListener("click", function (event) {
   var kind = this;
 
   const languages = ["english", "japanese"];
-  var long = languages[0];
+  var long = languages[1];
   var short = long.substring(0, 2);
 
   switch (kind.id) {
@@ -485,7 +1074,7 @@ document.querySelector(".icon").addEventListener("click", function (event) {
 
 function search(query) {
   const languages = ["english", "japanese"];
-  var long = languages[0];
+  var long = languages[1];
   var short = long.substring(0, 2);
 
   document.getElementById(
